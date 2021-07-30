2f850d1a1ab7d5a864cd24a46aba5c809312ace8
Pokachi <melodyhalftone@hotmail.com>
2021-07-29 23:39:39 -0700

    more smol changes

--- src/components/WanderingMerchantDetail.vue
+++ src/components/WanderingMerchantDetail.vue
@@ -1,9 +1,13 @@
 <template>
   <div v-if="merchantData && itemData">
-    <div v-for="item in itemData" :key="item.id" class="container d-flex h-100">
-        <ItemIcon :icon="item.image" :grade="item.grade" :sub-icon="item.subIcon" class="mr-3"/>
-        <font-awesome-icon class="mt-2" icon="angle-double-left" size="4x"></font-awesome-icon>
-        <ItemIcon :icon="item.currencyImage" :grade="0" :cost="item.cost" class="ml-3"/>
+    <div v-for="item in merchantData.items" :key="item.id" class="container d-flex h-100">
+        <ItemIcon :icon="itemData[item.id].image" :grade="itemData[item.id].grade" :sub-icon="itemData[item.id].subIcon" class="mr-3"/>
+        <font-awesome-icon v-if="!item.condition" class="mt-2" icon="angle-double-left" size="4x"></font-awesome-icon>
+        <div v-else>
+          <font-awesome-icon class="mt-0" icon="angle-double-left" size="4x" />
+          <span>{{item.condition}}</span>
+        </div>
+        <ItemIcon :icon="itemData[item.id].currencyImage" :grade="0" :cost="itemData[item.id].cost" class="ml-3"/>
     </div>
   </div>
 </template>
@@ -26,7 +30,7 @@ export default {
       this.merchantData = await import("@/assets/data/merchants/" + this.merchantId + ".json");
       this.itemData = {};
       for (const item of this.merchantData.items) {
-        this.itemData[item.itemId] = await import("@/assets/data/items/" + item.itemId + ".json");
+        this.itemData[item.id] = await import("@/assets/data/items/" + item.id + ".json");
       }
       this.$forceUpdate()
     } catch (e) {
