9b15eb995ebdf0824b528a88cad34db171c638ac
Pokachi <melodyhalftone@hotmail.com>
2021-07-30 21:54:21 -0700

    good check point for wip item

--- src/components/ItemIcon.vue
+++ src/components/ItemIcon.vue
@@ -1,45 +1,130 @@
 <template>
   <div>
-    <div class="position-relative fit-content">
-      <b-img :style="'background-image: url(./images/items/background/icon_grade_' + grade + '.png)'" class="item-icon" :src="icon" />
-      <b-img v-if="subIcon" class="item-sub-icon" :src="'./images/items/subicon/icon_' + subIcon + '.png'" />
+    <div :id="id" class="position-relative fit-content">
+      <b-img :style="'background-image: url(./images/items/background/icon_grade_' + data.grade + '.png)'" class="item-icon" :src="data.image" />
+      <b-img v-if="data.subIcon" class="item-sub-icon" :src="'./images/items/subicon/icon_' + data.subIcon + '.png'" />
       <span v-if="cost" class="item-cost"> {{cost}} </span>
     </div>
+
+    <b-tooltip custom-class="item-tooltip text-left" :target="id" triggers="hover">
+      <h4 :class="['font-grade-' + data.grade, 'title', 'mb-0']"> {{data.name}} </h4>
+      <hr class="bg-light mb-2 mt-0">
+      <div :class="['text-left', 'pl-3', 'item-icon-popup' + data.grade, 'mb-2']">
+        <b-img :src="data.image" />
+        <b-img v-if="data.subIcon" class="item-sub-icon-popup" :src="'./images/items/subicon/icon_' + data.subIcon + '.png'" />
+        <span :class="['font-grade-' + data.grade, 'item-type']"> {{ data.type}} </span>
+      </div>
+      <p v-if="data.bound" v-html="data.bound" class="text-left pl-3"/>
+      <p v-html="data.description" class="text-left pl-3 description"></p>
+      <p v-if="data.destruction" v-html="data.destruction" class="text-left pl-3" />
+    </b-tooltip>
   </div>
 </template>
 
 <script>
 export default {
   name: "ItemIcon",
-  props: ['grade', 'icon', 'subIcon', 'cost', 'itemName']
+  props: ['data', 'showName', 'cost', 'id']
 }
 </script>
 
+<style>
+.item-tooltip {
+  opacity: 1 !important;
+}
+
+/* tooltip background color */
+.item-tooltip .tooltip-inner {
+  background-color: #2d2d2d;
+  box-shadow: 0 3px 14px rgb(0 0 0 / 40%);
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+  padding-top: 0 !important;
+  min-width: 400px;
+}
+/* arrrow color */
+.item-tooltip .arrow::before {
+  border-top-color: #2d2d2d !important;
+}
+</style>
+
 <style scoped>
+.title {
+  background-color: black;
+  width: 100%;
+  padding-top: 0.75rem;
+  padding-bottom: 1rem;
+}
+
+.description {
+  color: #d2ba72;
+}
 
 .font-grade-0 {
-  color:white;
+  color: white;
+}
+.item-icon-popup0 {
+  background-image: radial-gradient(ellipse at bottom,
+  #ffffff40 0,
+  transparent 70%);
+  background-position: -10rem 0;
+  background-repeat: no-repeat;
 }
 
 .font-grade-1 {
-  color:#8df901
+  color: #8df901
+}
+.item-icon-popup1 {
+  background-image: radial-gradient(ellipse at bottom,
+  #8df90140 0,
+  transparent 70%);
+  background-position: -10rem 0;
+  background-repeat: no-repeat;
 }
 
 .font-grade-2 {
-  color:#00b0fa
+  color: #00b0fa
+}
+.item-icon-popup2 {
+  background-image: radial-gradient(ellipse at bottom,
+  #00b0fa40 0,
+  transparent 70%);
+  background-position: -10rem 0;
+  background-repeat: no-repeat;
 }
 
 .font-grade-3 {
-  color:#ba00f9
+  color: #ba00f9
+}
+.item-icon-popup3 {
+  background-image: radial-gradient(ellipse at bottom,
+  #ba00f940 0,
+  transparent 70%);
+  background-position: -10rem 0;
+  background-repeat: no-repeat;
 }
 
 .font-grade-4 {
-   color:#f99200
+   color: #f99200
  }
+.item-icon-popup4 {
+  background-image: radial-gradient(ellipse at bottom,
+  #f9920040 0,
+  transparent 70%);
+  background-position: -10rem 0;
+  background-repeat: no-repeat;
+}
 
 .font-grade-5 {
-  color:#fa5d00
+  color: #fa5d00
 }
+.item-icon-popup5 {
+   background-image: radial-gradient(ellipse at bottom,
+   #fa5d0040 0,
+   transparent 70%);
+   background-position: -10rem 0;
+   background-repeat: no-repeat;
+ }
 
 .fit-content {
   width: fit-content;
@@ -71,4 +156,18 @@ export default {
   bottom: 0;
   right: 0;
 }
+
+.item-sub-icon-popup {
+  position: relative;
+  display: inline-block;
+  width: 28px;
+  height: 28px;
+  bottom: -15px;
+  right: 30px;
+}
+
+.item-type {
+  font-weight: bold;
+}
+
 </style>
--- src/components/WanderingMerchantDetail.vue
+++ src/components/WanderingMerchantDetail.vue
@@ -1,13 +1,13 @@
 <template>
-  <div v-if="merchantData && itemData">
-    <div v-for="item in merchantData.items" :key="item.id" class="container d-flex h-100">
-        <ItemIcon :icon="itemData[item.id].image" :grade="itemData[item.id].grade" :sub-icon="itemData[item.id].subIcon" class="mr-3"/>
+  <div v-if="merchantData && Object.keys(itemData).length !== 0">
+    <div v-for="(item, index) in merchantData.items" :key="item.id" class="container d-flex h-100 mt-2">
+        <ItemIcon :data="itemData[item.id]" class="mr-3" :id="item.id + index.toString()" :key="item.id + index.toString()"/>
         <font-awesome-icon v-if="!item.condition" class="mt-2" icon="angle-double-left" size="4x"></font-awesome-icon>
         <div v-else>
           <font-awesome-icon class="mt-0" icon="angle-double-left" size="4x" />
           <span>{{item.condition}}</span>
         </div>
-        <ItemIcon :icon="itemData[item.id].currencyImage" :grade="0" :cost="itemData[item.id].cost" class="ml-3"/>
+        <ItemIcon :data="currencyData[itemData[item.id].currency]" :cost="itemData[item.id].cost" class="ml-3" :id="itemData[item.id].currency + index.toString()" :key="itemData[item.id].currency + index.toString()"/>
     </div>
   </div>
 </template>
@@ -22,20 +22,28 @@ export default {
   data: function () {
     return {
       merchantData: null,
-      itemData: null
+      itemData: null,
+      currencyData: null
     }
   },
   async created() {
     try {
       this.merchantData = await import("@/assets/data/merchants/" + this.merchantId + ".json");
       this.itemData = {};
+      this.currencyData = {}
       for (const item of this.merchantData.items) {
-        this.itemData[item.id] = await import("@/assets/data/items/" + item.id + ".json");
+        let itemData = await import("@/assets/data/items/" + item.id + ".json");
+        this.itemData[item.id] = itemData;
+        if (itemData.currency && !this.currencyData[itemData.currency]) {
+          this.currencyData[itemData.currency] = await import("@/assets/data/items/" + itemData.currency + ".json");
+        }
       }
+
       this.$forceUpdate()
     } catch (e) {
       this.merchantData = null;
       this.itemData = null;
+      this.currencyData = null;
     }
   }
 }
