b74638b522388ad7c670b3ba43ccd1c3c084e904
Pokachi <melodyhalftone@hotmail.com>
2021-12-06 23:19:08 -0800

    more wip affinity

--- src/components/AffinityDetail.vue
+++ src/components/AffinityDetail.vue
@@ -28,21 +28,21 @@
       </div>
     </div>
 
-    <b-tabs content-class="mt-1" active-nav-item-class="bg-dark text-light" fill>
-      <b-tab title="Requirement" active>
+    <b-tabs content-class="mt-1" active-nav-item-class="bg-dark text-light" justified>
+      <b-tab title="Requirements" active>
         <div class="d-flex flex-column">
           <h4 v-if="affinityData.condition.quest" class="mt-2">
             Quest
           </h4>
-          <div v-if="affinityData.condition.quest">
+          <b-link v-if="affinityData.condition.quest" :href="affinityData.condition.questLink" target="_blank" rel="noopener noreferrer">
             <b-img :src="'./images/icons/' + affinityData.condition.questType + '.png'" class="pb-1"/>
             <span :class="['d-inline-block', 'acquisition-text', affinityData.condition.questType, 'ml-1']">
               {{affinityData.condition.quest}}
             </span>
-          </div>
+          </b-link>
         </div>
       </b-tab>
-      <b-tab title="Reward">
+      <b-tab title="Rewards">
         <div class="d-flex flex-column">
           <div v-for="(rewardStage, index) of Object.keys(affinityData.rewards)"
                :key="rewardStage"
@@ -54,19 +54,7 @@
           </div>
         </div>
       </b-tab>
-      <b-tab title="Likes">
-        <div class="d-flex flex-column">
-          <h4 class="affinity-header-name">Songs</h4>
-          <div v-for="song of affinityData.likes.songs" :key="song.id" class="darker-bg mb-2 pl-3 pr-3 pt-1">
-            <song-detail :song-id="song.id" :points="song.points" class="mb-2 mt-2"/>
-          </div>
-          <h4 class="affinity-header-name">Emotes</h4>
-          <div v-for="emote of affinityData.likes.emotes" :key="emote.id" class="darker-bg mb-2 pl-3 pr-3 pt-1">
-            <emote-detail :emote-id="emote.id" :points="emote.points" class="mb-2 mt-2"/>
-          </div>
-        </div>
-      </b-tab>
-      <b-tab title="Stage" :set="totalExp = 0">
+      <b-tab title="Stages" :set="totalExp = 0">
         <div class="d-flex flex-column flex-wrap">
           <div class="d-flex flex-row">
             <h6 class="border m-0 p-1 affinity-level-header"> Stage </h6>
@@ -74,9 +62,77 @@
             <h6 class="border m-0 p-1 affinity-level-header"> Total Points </h6>
           </div>
           <div v-for="level of affinityData.levels" :key="level.name" class="d-flex flex-row" :set="totalExp= totalExp + level.expRequired">
-            <p :class="['border m-0 p-1', 'affinity-' + level.stage]">{{ level.name }}</p>
-            <p :class="['border m-0 p-1', 'affinity-' + level.stage]">{{ level.expRequired }}</p>
-            <p :class="['border m-0 p-1', 'affinity-' + level.stage]">{{ totalExp }}</p>
+            <p :class="['border m-0 p-1', 'affinity-level-header', 'affinity-' + level.stage]">{{ level.name }}</p>
+            <p :class="['border m-0 p-1', 'affinity-level-header', 'affinity-' + level.stage]">{{ level.expRequired }}</p>
+            <p :class="['border m-0 p-1', 'affinity-level-header', 'affinity-' + level.stage]">{{ totalExp }}</p>
+          </div>
+        </div>
+      </b-tab>
+      <b-tab title="Quests">
+        <div v-if="affinityData.quests" class="d-flex flex-column flex-wrap">
+          <div class="d-flex flex-row">
+            <h6 class="border m-0 p-1 quest-header"> Quest </h6>
+            <h6 class="border m-0 p-1 stage-header"> Stage </h6>
+            <h6 class="border m-0 p-1 stats-header"> Stats </h6>
+          </div>
+          <div v-for="quest of affinityData.quests" :key="quest.name" class="d-flex flex-row">
+            <b-link v-if="quest.link" :href="quest.link" target="_blank" rel="noopener noreferrer" class="border m-0 p-1 quest-header">
+              <b-img :src="'./images/icons/' + quest.type + '.png'" class="pb-1"/>
+              <span :class="['d-inline-block', quest.type, 'ml-1']">
+                {{quest.name}}
+              </span>
+            </b-link>
+            <div v-else class="border m-0 quest-header" style="padding-top: 0.4rem">
+              <b-img :src="'./images/icons/' + quest.type + '.png'" class="pb-1"/>
+              <span :class="['d-inline-block', 'ml-1']">
+                {{quest.name}}
+              </span>
+            </div>
+            <div style="padding-top: 0.4rem" v-if="quest.conditions && quest.conditions.stage" :class="['border', 'm-0', 'stage-header', 'affinity-' + quest.conditions.stageId]">
+              {{quest.conditions.stage}}
+            </div>
+            <div class="border stats-header d-flex">
+              <div v-if="quest.conditions.charisma" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+                <b-img style="width: 18px; height: 18px" :src="'./images/other/charisma.png'" alt="Charisma" class="mt-1"/>
+                <span class="stats-number"> {{quest.conditions.charisma}} </span>
+              </div>
+              <div v-if="quest.conditions.wisdom" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+                <b-img style="width: 18px; height: 18px" :src="'./images/other/wisdom.png'" alt="Wisdom" class="mt-1"/>
+                <span class="stats-number"> {{quest.conditions.wisdom}} </span>
+              </div>
+              <div v-if="quest.conditions.courage" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+                <b-img style="width: 18px; height: 18px" :src="'./images/other/courage.png'" alt="Courage" class="mt-1"/>
+                <span class="stats-number"> {{quest.conditions.courage}} </span>
+              </div>
+              <div  v-if="quest.conditions.kindness" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+                <b-img style="width: 18px; height: 18px" :src="'./images/other/kindness.png'" alt="Kindness" class="mt-1"/>
+                <span class="stats-number"> {{quest.conditions.kindness}} </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-else>
+          <span :class="['d-inline-block', 'acquisition-text', 'ml-1']">
+            No Quests or Dialogue Available
+          </span>
+        </div>
+      </b-tab>
+      <b-tab title="Songs">
+        <div class="d-flex flex-column">
+          <div v-for="song of affinityData.likes.songs" :key="song.id" class="darker-bg mb-2 pl-3 pr-3 pt-1">
+            <song-detail :song-id="song.id" :points="song.points" class="mb-2 mt-2"/>
+          </div>
+        </div>
+      </b-tab>
+      <b-tab title="Emotes">
+        <div v-for="emote of affinityData.likes.emotes" :key="emote.id" class="darker-bg mb-2 pl-3 pr-3 pt-1">
+          <emote-detail :emote-id="emote.id" :points="emote.points" class="mb-2 mt-2"/>
+        </div>
+      </b-tab>
+      <b-tab title="Gifts">
+        <div class="d-flex flex-wrap">
+          <div v-for="gift of affinityData.likes.gifts" :key="gift.id" class="m-1">
+            <gift-detail :gift-id="gift.id" :points="gift.points" :conditions="gift.conditions"/>
           </div>
         </div>
       </b-tab>
@@ -88,10 +144,11 @@
 import ItemIcon from "@/components/ItemIcon";
 import SongDetail from "@/components/SongDetail";
 import EmoteDetail from "@/components/EmoteDetail";
+import GiftDetail from "@/components/GiftDetail";
 
 export default {
   name: "AffinityDetail",
-  components: {SongDetail, EmoteDetail, ItemIcon},
+  components: {SongDetail, EmoteDetail, GiftDetail, ItemIcon},
   props: ['affinityId', 'disambiguator'],
   data: function () {
     return {
@@ -123,6 +180,32 @@ export default {
 </script>
 
 <style scoped>
+.stats-number {
+  position: absolute;
+  display: inline-block;
+  width: auto;
+  height: auto;
+  bottom: 0;
+  right: 0;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  font-size: smaller;
+  font-weight: bold;
+}
+
+.quest-header {
+  width: 160px;
+  height: 32px;
+}
+
+.stage-header {
+  width: 100px;
+  height: 32px;
+}
+
+.stats-header {
+  width: 109px;
+  height: 32px;
+}
 
 .affinity-level-header {
   width: 120px;
@@ -143,22 +226,16 @@ export default {
 
 .affinity-normal {
   color: #cdb344;
-  width: 120px;
-  height: fit-content;
   text-transform: capitalize;
 }
 
 .affinity-interested {
   color: #7ec543;
-  width: 120px;
-  height: fit-content;
   text-transform: capitalize;
 }
 
 .affinity-friendly {
   color: #51c5b8;
-  width: 120px;
-  height: fit-content;
   text-transform: capitalize;
 }
 
---
+++ src/components/GiftDetail.vue
@@ -0,0 +1,66 @@
+<template>
+  <div v-if="itemData">
+    <div class="d-flex">
+      <item-icon :data="itemData" :id="giftId + uuidv4()" :key="itemData.id" size="0.7"/>
+      <div class="d-flex flex-column ml-2 text-left">
+        <p v-if="points"  class="m-0 points"> {{points}} Affinity</p>
+        <div v-if="conditions" class="d-flex">
+          <div v-if="conditions.charisma" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+            <b-img style="width: 18px; height: 18px" :src="'./images/other/charisma.png'" alt="Charisma" class="mt-1"/>
+            <span class="stats-number"> {{conditions.charisma}} </span>
+          </div>
+          <div v-if="conditions.wisdom" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+            <b-img style="width: 18px; height: 18px" :src="'./images/other/wisdom.png'" alt="Wisdom" class="mt-1"/>
+            <span class="stats-number"> {{conditions.wisdom}} </span>
+          </div>
+          <div v-if="conditions.courage" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+            <b-img style="width: 18px; height: 18px" :src="'./images/other/courage.png'" alt="Courage" class="mt-1"/>
+            <span class="stats-number"> {{conditions.courage}} </span>
+          </div>
+          <div  v-if="conditions.kindness" class="position-relative ml-auto mr-auto" style="width: 25px; height: 25px">
+            <b-img style="width: 18px; height: 18px" :src="'./images/other/kindness.png'" alt="Kindness" class="mt-1"/>
+            <span class="stats-number"> {{conditions.kindness}} </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemIcon from "@/components/ItemIcon";
+import '@/assets/css/main.css';
+
+export default {
+  name: "GiftDetail",
+  props: ["giftId", "points", "conditions"],
+  components: {ItemIcon},
+  data: function() {
+    return {
+      itemData: null
+    }
+  },
+  async created() {
+    this.itemData = await import("@/assets/data/items/" + this.giftId + ".json");
+    this.$forceUpdate()
+  }
+}
+</script>
+
+<style scoped>
+.stats-number {
+  position: absolute;
+  display: inline-block;
+  width: auto;
+  height: auto;
+  bottom: 0;
+  right: 0;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  font-size: smaller;
+  font-weight: bold;
+}
+
+.points {
+  color: #19a7e5;
+}
+</style>
